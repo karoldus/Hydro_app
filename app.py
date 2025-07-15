be38d5d4d6eb8a25384c3b13ce10a6cdfda5e140
karol <01158930@pw.edu.pl>
2025-07-15 18:25:54 +0200

    allow_unsafe_werkzeug

--- app.py
+++ app.py
@@ -216,4 +216,4 @@ if __name__ == '__main__':
     else:
         print("Telegram notifications: DISABLED (configure TELEGRAM_BOT_TOKEN and TELEGRAM_CHAT_ID)")
     
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
